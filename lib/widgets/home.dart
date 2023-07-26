3488605f42ed6f2d60fda665d90af967cb56a085
GuiParpineli <guiparpineli@hotmail.com>
2023-07-25 22:45:50 -0300

    ASSERT: change for layout builder in home

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,115 +1,32 @@
 import 'package:flutter/material.dart';
 
-List<String> titles = <String>[
-  'Matriz - Plano de Contas',
-  'Beach',
-  'Sunny',
-];
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = Colors.white;
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-    const int tabsCount = 3;
-
-    return DefaultTabController(
-      initialIndex: 1,
-      length: tabsCount,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'Clear Cont',
-            style: TextStyle(color: Colors.white),
-          ),
-          notificationPredicate: (ScrollNotification notification) {
-            return notification.depth == 1;
-          },
-          scrolledUnderElevation: 4.0,
-          shadowColor: Theme.of(context).shadowColor,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Perfil",
-                      style: TextStyle(color: Colors.white),
-                    ),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth < 720) {
+          return Scaffold();
+        } else {
+          return Container(
+            height: 20,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: double.infinity,
+                  child: Text(
+                    'Hello World!',
                   ),
-                  Icon(
-                    Icons.person,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            )
-          ],
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: titles[0],
-              ),
-              Tab(
-                text: titles[1],
-              ),
-              Tab(
-                text: titles[2],
-              ),
-              Tab(
-                text: titles[2],
-              ),
-              Tab(
-                text: titles[2],
-              ),
-              Tab(
-                text: titles[2],
-              ),
-              Tab(
-                text: titles[2],
-              ),
-            ],
-            labelColor: Colors.white,
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[0]} $index'),
-                );
-              },
+                ),
+              ],
             ),
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[1]} $index'),
-                );
-              },
-            ),
-            ListView.builder(
-              itemCount: 25,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
-                  title: Text('${titles[2]} $index'),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
